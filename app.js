7c687564864ab88c77b6d127b101839848c6ed2a
Taras Shevchenko <shev-taras@ya.ru>
2022-04-01 11:51:59 +0400

    Preparing the application for hosting

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ app.use(express.json({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/images", require("./routes/images.routes"));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build' , 'index.html'))
+  })
+}
 const PORT = config.get("port") || 5000;
 
 async function start() {
